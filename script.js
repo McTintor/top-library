5b02a10c24a9ffb63c2be44783eb79760342fd6b
McTintor <igortintor99@gmail.com>
2024-02-26 18:30:54 +0100

    finished design and made dialog/modal

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ function Book() {
 function addBookToLibrary() {
 
 }
+
+
+const openButton = document.querySelector(".open");
+const closeButton = document.querySelector(".close");
+const modal = document.querySelector(".modal");
+const addButton = document.querySelector(".add");
+
+openButton.addEventListener("click", () => {
+    modal.showModal();
+});
+
+closeButton.addEventListener("click", () => {
+    modal.close();
+})
